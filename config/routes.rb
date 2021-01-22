079820ff5b297493ed66c6bbde64d9e497bef0d2
Simon Jacobs <simonadlerjacobs@gmail.com>
2021-01-22 17:49:07 -0500

    UserController added, StaticController added

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  get 'user/index'
+  get 'user/show'
+  get 'user/new'
+  root 'static#welcome'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+# get '/' to: 
 end
